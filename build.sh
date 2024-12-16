0636af1e24e448854eb6c62254ed2365554967f4
SANTHOSH <sandysanthosh991@gmail.com>
2024-12-16 10:56:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker-compose build 
+SERVICE_NAME="app"
+IMAGE_NAME="new"
+TAG="latest"
+docker-compose build $SERVICE_NAME
+IMAGE_ID=$(docker images --filter=reference="$(SERVICE_NAME}*" --format "{{.ID}}" | head -n 1)
+docker tag $IMAGE_ID $IMAGE_NAME:$TAG
