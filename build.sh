5f428c140b6533f86cc3e699421f437ac0815c15
SANTHOSH <sandysanthosh991@gmail.com>
2024-12-16 11:09:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-SERVICE_NAME="app"
-IMAGE_NAME="new"
-docker-compose build $SERVICE_NAME
-IMAGE_ID=$(docker images --filter=reference="$(SERVICE_NAME}*" --format"{{.ID}}"| head -n 1)
-docker tag $IMAGE_ID $IMAGE_NAME
+docker build -t my-nginx-app .
